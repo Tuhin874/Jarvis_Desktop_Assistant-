860b62779bb03ffe9a55748e1effe54b1c0c085c
Tuhin874 <tuhin.sarkar874@gmail.com>
2024-08-21 19:53:29 +0530

    virtual environment added

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import eel
+
+
+eel.init("Frontend")
+
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+
+eel.start('index.html', mode=None, host='localhost', block=True)
